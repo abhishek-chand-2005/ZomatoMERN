3c8acad16f4c361a24202ea4579b5719de859b97
Abhishek Chand <0101abhi0101@gmail.com>
2025-10-03 06:45:35 +0530

    Add login functionality

--- BACKEND/src/controllers/auth.controller.js
+++ BACKEND/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/auth.model')
 const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const { default: mongoose } = require('mongoose');
 
 async function registerUser(req, res) {
     const {fullName, email, password} = req.body;
@@ -37,7 +38,43 @@ async function registerUser(req, res) {
     })
 }
 
+async function loginUser(req, res) {
+    const { email, password} = req.body;
+    const user = await userModel.findOne({
+        email
+    })
+    if(!user){
+        return res.status(400).json({
+            message: "Invalid credintial."
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+    if(!isPasswordValid){
+        return res.status(400).json({
+            message: "Invalid credintial."
+    })}
+
+    const token = jwt.sign({
+        id: user._id,
+    }, process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+
+    res.status(200).json({
+        message:"User loggedIn successfully.",
+
+        user:{
+            _id: user._id,
+            email: user.email,
+            fullName: user.fullName
+        }
+    })
+
+}
+
 
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 }
